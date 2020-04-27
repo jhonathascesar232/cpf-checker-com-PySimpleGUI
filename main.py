d1a34abb100e58abcaa49d5608a1411c805fca80
Jhonathas César <jhonathascesar232@gmail.com>
2020-04-27 18:09:32 -0300

    App válidador de CPF com PySimpleGUI

---
+++ main.py
@@ -0,0 +1,51 @@
+import PySimpleGUI as sg
+from cpf import isCpfValid
+
+class Tela:
+
+    def __init__(self):
+        sg.change_look_and_feel('Black')
+
+        # layout
+        lay = [
+            [sg.Text('CPF:'), sg.InputText(key='cpf')],
+            [sg.Button('Check', key='checker'), sg.Text(' '*20, key='validador')],
+        ]
+
+        self.jan = sg.Window('CPF Checker').layout(lay)
+
+    def iniciar(self):
+        while 1:
+            self.btn, self.values = self.jan.Read()
+
+            CPF = self.values['cpf']
+
+            if self.btn == 'checker':
+                print('\n** CLICK')
+                print(f'\n** CPF:\n** {CPF}')
+
+                if isCpfValid(CPF):
+                    self.jan['validador'].update('CPF válido!', text_color='green')
+                else:
+                    self.jan['validador'].update('CPF inválido!', text_color='red')
+
+            if self.btn is None:
+                break
+
+##    def verdade(self):
+##        btn = sg('CPF válido!', text_color='green')
+##        return btn
+##
+##    def falso(self):
+##        btn = sg('CPF inválido!', text_color='red')
+##        return btn
+
+
+def main():
+    app = Tela()
+    app.iniciar()
+
+
+# programa principal
+if __name__ == '__main__':
+    main()
